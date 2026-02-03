0b40de9f4292b055dadf05414c16e0ed73ac8687
klismanmelo <klismanfdm@gmail.com>
2026-02-03 19:53:12 -0300

    server with zod & jwt & swagger ok

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,8 +1,41 @@
 import { fastify } from "fastify"
+import { fastifyJwt } from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySaggerUI from '@fastify/swagger-ui'
+
+import {
+    jsonSchemaTransform,
+    serializerCompiler,
+    validatorCompiler,
+    ZodTypeProvider
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
-server.listen({ port: 3333 }, (err, address) => {
+server.withTypeProvider<ZodTypeProvider>()
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: 'Nexi Project API',
+            version: '1.0.0',
+        },
+        servers: []
+    },
+    transform: jsonSchemaTransform,
+})
+
+server.register(fastifySaggerUI, {
+    routePrefix: '/docs'
+})
+
+server.register(fastifyJwt, {
+    secret: 'super-secret-key',
+})
+
+server.listen({ port: 3332 }, (err, address) => {
     if (err) {
         console.error(err)
         process.exit(1)
